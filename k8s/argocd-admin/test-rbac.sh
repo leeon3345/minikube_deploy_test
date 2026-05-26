6ebf2f8ee67daeec94c945f446f2e8306f60812c
Lee on 32203345 <157787044+leeon3345@users.noreply.github.com>
2026-05-27 00:11:01 +0900

    chore: grad-deploy [k8s/argocd-admin/test-rbac.sh]

--- k8s/argocd-admin/test-rbac.sh
+++ k8s/argocd-admin/test-rbac.sh
@@ -8,7 +8,7 @@ set -e
 PROJ=$1
 TOKEN=$2
 ARGOCD_SERVER="https://localhost:8080"
-EXPECTED_PROJECT_NAME="my-app-project"
+EXPECTED_PROJECT_NAME="minikube_deploy_test-project"
 ACTUAL_PROJECT_NAME="$PROJ-project"
 
 if [ -z "$PROJ" ] || [ -z "$TOKEN" ]; then
