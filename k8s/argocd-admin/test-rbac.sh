3bb7fe82740e981cf08e448e29b3938bdff3aad7
Lee on 32203345 <157787044+leeon3345@users.noreply.github.com>
2026-05-27 01:09:55 +0900

    chore: grad-deploy [k8s/argocd-admin/test-rbac.sh]

--- k8s/argocd-admin/test-rbac.sh
+++ k8s/argocd-admin/test-rbac.sh
@@ -7,7 +7,7 @@ set -e
 
 PROJ=$1
 TOKEN=$2
-ARGOCD_SERVER="johnston-initial-awareness-particle.trycloudflare.com"
+ARGOCD_SERVER="https://johnston-initial-awareness-particle.trycloudflare.com"
 EXPECTED_PROJECT_NAME="minikube-deploy-test-project"
 ACTUAL_PROJECT_NAME="$PROJ-project"
 
